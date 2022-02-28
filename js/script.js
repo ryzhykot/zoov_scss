59241fc461cd6f6e151818c0840bf281c63304f9
Vladyslav <ryzhykovvladyslav@gmail.com>
2022-02-28 22:58:54 +0300

    made the header fixed after scrolling the home screen

--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
-'use strict';
+window.onload = function () {
+  // MENU-BURGER
 
-// MENU-BURGER
+  const iconMenu = document.querySelector('.menu__icon');
+  const bodyMenu = document.querySelector('.menu__body');
 
-const iconMenu = document.querySelector('.menu__icon');
-const bodyMenu = document.querySelector('.menu__body');
+  if (iconMenu) {
+    iconMenu.addEventListener('click', function(e) {
+    /* document.body.classList.toggle('_lock'); */
+    iconMenu.classList.toggle('active');
+    bodyMenu.classList.toggle('active');
+  });
+  }
 
-if (iconMenu) {
-  iconMenu.addEventListener('click', function(e) {
-  /* document.body.classList.toggle('_lock'); */
-  iconMenu.classList.toggle('active');
-  bodyMenu.classList.toggle('active');
-});
-}
+  // HEADER
+
+  const headerElement = document.querySelector('.header');
+
+  const callback = function (entries, observer) {
+    if (entries[0].isIntersecting) {
+      headerElement.classList.remove('_scroll');
+    } else {
+      headerElement.classList.add('_scroll');
+    }
+  };
+
+  const headerObserver = new IntersectionObserver(callback);
+  headerObserver.observe(headerElement); // метод следит за объектом header
+
+};
+
+
 
 
 
