b64212c8cbe698a1ec76f334a3192f45365b0623
Vladyslav <ryzhykovvladyslav@gmail.com>
2022-03-18 10:41:24 +0300

    site version made with gulp

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-@@include('files/sliders.js', {})
-@@include('files/regular.js', {})
-@@include('files/script.js', {})
-@@include('files/forms.js', {})
-
-
-
-
-
--- src/js/vendors.js
+++
@@ -1,4 +0,0 @@
-@@include('libs/swiper.min.js', {})
-@@include('libs/inputmask.min.js', {})
-@@include('libs/lightgallery.min.js', {})
-
